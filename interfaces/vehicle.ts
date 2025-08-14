582bdcbd91343105377c2bda64fdd61624fe4936
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2025-08-14 07:24:11 -0500

    first commit

---
+++ interfaces/vehicle.ts
@@ -0,0 +1,14 @@
+export interface Vehicle {
+  id: number
+  plate: string
+  make: string
+  model: string
+  year: number
+  vin: string
+  color: string
+  price: number
+  odometer: number
+  condition: string
+  transmission: string
+  fuel_type: string
+}
